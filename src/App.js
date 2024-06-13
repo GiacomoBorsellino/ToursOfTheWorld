ea446f671c4059d9f739cc38e75452b4b0502420
Giacomo Borsellino <Giacomo.Borsellino@avrgroup.it>
2024-06-13 14:41:48 +0200

    fix fetch

--- src/App.js
+++ src/App.js
@@ -1,49 +1,44 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
+import cities from "./cities";
+import Style from "./Style.module.css";
 
-import Style from './Style.module.css'
-
-const url = 'https://course-api.com/react-tours-project';
+// const url = 'https://course-api.com/react-tours-project';
 
 const App = () => {
-
-const [tours, setTours] = useState([]);
-
+  const [tours, setTours] = useState([]);
 
   const getTours = async () => {
-    const response = await fetch(url)
-    const tours = await response.json();
-    setTours(tours)
-  }
+    // const response = await fetch(url)
+    // const tours = await response.json();
+    setTours(cities);
+  };
 
-  useEffect( () => { 
+  useEffect(() => {
     getTours();
-  }, [])
+  }, []);
 
-  return(
+  return (
     <React.Fragment>
-      <h1>Tours of the World</h1>  
+      <h1>Tours of the World</h1>
 
       <ul>
-      {tours.map( (tour) => {
-
-        const {id, name, image, info, price} = tour;
-        console.log(tour)
-        return(
-
-        <li key={id}>
-          <img src={image} alt= {name}/>
-          <div>
-            <h4>Journey: {name}</h4>
-            <h3 className={Style['price']}>Price: {price}</h3>
-            <h3>Info: {info}</h3>
-          </div>
-        </li>    
-        ) 
-      } )}      
+        {tours.map((tour) => {
+          const { id, name, image, info, price } = tour;
+          console.log(tour);
+          return (
+            <li key={id}>
+              <img src={image} alt={name} />
+              <div>
+                <h4>Journey: {name}</h4>
+                <h3 className={Style["price"]}>Price: {price}</h3>
+                <h3>Info: {info}</h3>
+              </div>
+            </li>
+          );
+        })}
       </ul>
-
     </React.Fragment>
-  )
+  );
 };
 
 export default App;
